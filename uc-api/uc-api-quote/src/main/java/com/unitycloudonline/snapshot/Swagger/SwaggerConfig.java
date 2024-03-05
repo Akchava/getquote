d225ae039fc7ed6c2e9b7db4e5abab9bfca5aa67
Anilkumar chava <anilkumarchava@Anilkumars-Air.attlocal.net>
2024-03-05 14:17:11 -0600

    updated swagerconfig file

--- uc-api/uc-api-quote/src/main/java/com/unitycloudonline/snapshot/Swagger/SwaggerConfig.java
+++ uc-api/uc-api-quote/src/main/java/com/unitycloudonline/snapshot/Swagger/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.unitycloudonline.snapshot.Swagger;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -8,9 +9,11 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
 @Configuration
 @EnableSwagger2
-public class Swagger {
+
+public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -20,4 +23,3 @@ public class Swagger {
                 .build();
     }
 }
-
